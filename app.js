3caebde19c362e57c1ad6e236b61fc972786a3b2
azooz alharte <zezo50115@protonmail.com>
2018-08-08 01:26:21 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs')
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'))
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method}: ${req.ip}: ${req.url}`
+  console.log(log);
+  fs.appendFile('info.log', log + '\n', (err) => {
+    if (err) {
+      console.log(err);
+    }
+  })
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('error.hbs')
+})
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear() ;
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    titlePage: 'Home Page',
+    name: 'azooz alharte',
+    country: "saudi arabi",
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    titlePage: 'about page',
+  });
+});
+
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Error 404'
+  });
+});
+
+app.listen(3000, () => {
+  console.log("server up on port 3000 ");
+});
